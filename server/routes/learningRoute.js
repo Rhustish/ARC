d0c1909de08cec10f048c6b1311652a6761bbfc3
Bhavy Bhatnagar <50353779+Rhustish@users.noreply.github.com>
2023-04-15 00:26:28 +0530

    Delete server directory

--- server/routes/learningRoute.js
+++
@@ -1,8 +0,0 @@
-import  express  from "express";
-import {sendLearningTitImg , playLearningAudio} from "../controllers/learningControllers.js"
-const router = express.Router();
-
-router.get("/",sendLearningTitImg)
-router.get("/:id",playLearningAudio)
-
-export default router;
