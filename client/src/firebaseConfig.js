69ea14d64701276fb81afb27be934ab6720442c0
Rhustish <bbhatnagar27@gmail.com>
2023-04-22 00:27:35 +0530

    logout works (why u use hashrouter)

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,7 +1,6 @@
 import {getAuth} from "firebase/auth"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,6 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 
