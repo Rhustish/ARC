bc24c5f30dd97991491d03ec3620c1ec9b45f952
Rhustish <bbhatnagar27@gmail.com>
2023-04-22 19:07:02 +0530

    profile page template skelly

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home'
 import Arrival from './pages/Arrival/Arrival'
 import Signup from './pages/Signup/Signup'
 import TestPage1 from './pages/TestPage1/TestPage1'
+import Profile from './pages/Profile/Profile'
 
 const AllRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AllRoutes = () => {
             <Route path='/' element={<Arrival/>}/>
             <Route path='/Home' element={<Home/>}/>
             <Route path='/Signup' element={<Signup/>}/>
+            <Route path='/profile' element={<Profile/>}/>
             <Route path='/Test1/Level1' element={<TestPage1/>}/>
             <Route path='/Test1/Level2' element={<TestPage1/>}/>
             <Route path='/Test1/Level3' element={<TestPage1/>}/>
